96e30374f7194377b5ab6cc942e9eb7160c5c429
Siun  Maksim <myrdym@rambler.ru>
2021-11-05 13:32:26 +0300

    UI add

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,47 +1,33 @@
-/**
- * Класс Modal отвечает за
- * управление всплывающими окнами.
- * В первую очередь это открытие или
- * закрытие имеющихся окон
- * */
+
 class Modal {
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью Modal.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
-  constructor(element){
-
+  constructor(element) {
+    if (element === undefined) {
+      throw new Error("пустой элемент")
+    } else {
+      this.element = element;
+      this.registerEvents();
+    }
   }
-
-  /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
-   * */
-  registerEvents() {
-
-  }
-
-  /**
-   * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
-   * */
-  onClose(e) {
-
-  }
-  /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
-   * */
+  
   open() {
-
+    this.element.style.display = "block";
   }
-  /**
-   * Закрывает окно: удаляет CSS-свойство display
-   * */
-  close(){
 
+  close(){
+    this.element.style.display = "none";
+  }
+  
+  onClose(e) {
+    this.close();
+    e.preventDefault();
+  }
+
+  registerEvents() {
+    const dataDismiss = Array.from(this.element.querySelectorAll('[data-dismiss="modal"]'));
+    dataDismiss.forEach(elem => {
+      elem.addEventListener('click', () => {
+        this.onClose();
+      });
+    })
   }
 }
--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -12,23 +12,35 @@ class Sidebar {
     this.initToggleButton();
   }
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
   static initToggleButton() {
-
+    const sidebarMini = document.querySelector(".sidebar-mini");
+    const sidebarToggle = document.querySelector(".sidebar-toggle");
+    sidebarToggle.addEventListener("click", ()=>{
+      sidebarMini.classList.toggle("sidebar-open");
+      sidebarMini.classList.toggle("sidebar-collapse");
+    })
   }
-
-  /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
-   * */
+  
   static initAuthLinks() {
+    const menuItemRegister = document.querySelector(".menu-item_register");
+    menuItemRegister.addEventListener("click", ()=> {
+      const modal = App.getModal("register");
+      modal.open();
+    });
 
+    const menuItemLogin = document.querySelector(".menu-item_login");
+    menuItemLogin.addEventListener("click", ()=> {
+      const modal = App.getModal("login");
+      modal.open();
+    });
+
+    const menuItemLogout = document.querySelector(".menu-item_logout");
+    menuItemLogout.addEventListener("click", ()=> {
+      const modal = User.logout({}, (err, response) => {
+        if (response.success = true) {
+          App.setState('init');
+        }
+      }) 
+    });
   }
 }
